16ac6bcd5ee51f48b1932e049dac5dc5deaa331f
Lucas Mudo de Araujo <lucasmud@microsoft.com>
2023-02-02 13:33:51 -0800

    Day 12 part 2

--- 2021/Day12.cpp
+++ 2021/Day12.cpp
@@ -14,11 +14,14 @@ namespace Day12 {
 
 	class Graph {
 	private:
-		vector<vector<string>> allValidPaths;
+		int allValidPathsCounter;
 		unordered_map<string, vector<string>> _graph;
+		bool isPart2;
 
 	public:
-		Graph(string filename) {
+		Graph(string filename, bool part2 = false) {
+			isPart2 = part2;
+			allValidPathsCounter = 0;
 			ifstream file(filename);
 
 			if (file.is_open()) {
@@ -39,79 +42,83 @@ namespace Day12 {
 		// Find all possible paths from start -> end 
 		// traversing the graph in DFS fashion
 		void findAllPaths(string start, string end) {
-			vector<string> path;
-			path.push_back(start);
-			traverseGraph(start, end, path);
+			unordered_map<string, int> visit_counts;
+			traverseGraph(start, end, visit_counts);
 		}
 
-		void traverseGraph(string curr, string dest, vector<string>& path) {
+		void traverseGraph(string curr, string dest, unordered_map<string, int>& visit_counts) {
 			if (curr == dest) {
-				allValidPaths.push_back(path);
+				allValidPathsCounter++;
 			}
 			else {
 				for (const auto& node : _graph[curr]) {
 					// Recursively add all unvisited notes to curr path 
-					if (_canVisitNode(node, path)) {
-						path.push_back(node);
-						traverseGraph(node, dest, path);
-						path.pop_back();
+					if (_canVisitNode(node, visit_counts)) {
+						visit_counts[node]++;
+						traverseGraph(node, dest, visit_counts);
+						visit_counts[node]--;
 					}
 				}
 			}
 		}
 
 		// A lowercase node can be visit only once. Uppercase nodes can be re-visited 
-		bool _canVisitNode(string candidate, vector<string>& path) {
+		bool _canVisitNode(string candidate, unordered_map<string, int>& visit_counts) {
+			if (candidate == "start") {
+				return false;
+			}
+			// Check for small caves
 			char ch = candidate[0];
-			// Check if it is lowercase
-			if (islower(ch)) {
-				// Check if its part of the path already
-				if (std::find(path.begin(), path.end(), candidate) != path.end()){
+			// In part 2 one can only visit a *single* small cave twice. 
+			int visitsAllowed = isPart2 ? (isAnySmallCaveVisitedTwice(visit_counts) ? 1 : 2) : 1;
+			if (islower(ch) && visit_counts.find(candidate) != visit_counts.end()) {
+				if (visit_counts[candidate] < visitsAllowed)
+					return true;
+				else
 					return false;
-				}
 			}
 			return true; 
 		}
+		
+		// Returns true if a small cave has already been visited twice in the curr path
+		bool isAnySmallCaveVisitedTwice(unordered_map<string, int>& visit_counts) {
+			for (auto& it : visit_counts) {
+				if (islower(it.first[0]) && it.second > 1)
+					return true;
+			}
+			return false;
+		
+		}
 
 		// Returns how many paths visit small caves at most once (answer to pt. 1)
-		int countSmallCaves(string start = "start", string end = "end") {
-			findAllPaths(start, end);
-
-			int totalCount = 0;
-			for (auto path : allValidPaths) {
-				int _count = 0;
-				for (int i = 1; i < path.size() - 1; i++) {
-					char ch = path[i][0];
-					if (islower(ch))
-						_count++;
-				}
-				if (_count < 2)
-					totalCount++;
-			}
-
-			return totalCount;
+		int getPathsCount() {
+			if (allValidPathsCounter == 0)
+				findAllPaths("start", "end");
+			
+			return allValidPathsCounter;
 		}
 
 	};
 
 	int main1(string filename = "2021/data/day12.txt") {
-		Graph g(filename);
-		return g.countSmallCaves();
+		Graph g(filename, false);
+		return g.getPathsCount();
 	}
 
 	int main2(string filename = "2021/data/day12.txt") {
-		return 0;
+		Graph g(filename, true);
+		return g.getPathsCount();
 	}
 
 	void test() {
-		int answer = 7;
+		int answer = 19;
 		int my_response = main1("2021/data/day12-test.txt");
 		cout << "My response: " << my_response << "  Expected: " << answer << "\n\n";
 	}
 
 	void printAnswers() {
 		cout << "Validation: \n";
-		//test();
+		test();
 		cout << "First part: " << main1() << "\n";
 		cout << "Second part: " << main2() << "\n";
 	}
